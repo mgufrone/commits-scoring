41822fb2a9e4e3c824f7df8780ce26ad43b76f15
Mochamad Gufron <mgufronefendi@gmail.com>
2017-03-08 23:13:02 +0700

    update environment configuration

--- config/deploy.rb
+++ config/deploy.rb
@@ -32,10 +32,17 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
 # Default value for keep_releases is 5
-# set :keep_releases, 5
-
+set :keep_releases, 5
+namespace :bundler do
+  after :install, :environment do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      within release_path do
+        execute :mv, ".env.production .env"
+      end
+    end
+  end
+end
 namespace :deploy do
-
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
       # Here we can do anything such as:
