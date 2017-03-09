2cec59082df647738de65d9d328a4807c14c0812
Mochamad Gufron <mgufronefendi@gmail.com>
2017-03-09 07:25:38 +0700

    better reporting

--- app/bots/summary_bot.rb
+++ app/bots/summary_bot.rb
@@ -3,7 +3,7 @@ class SummaryBot
         @client = client
     end
     def pattern
-        /(?<action>summary)|(?<user>[\@a-zA-Z\d]+)\'s?\s(?<action>performance)/imx
+        /(?<action>summary|report)|((?<user>[\@a-zA-Z\d]+)\'s?\sperformance)/imx
     end
     def order_pattern
         /(order|sort)\s?by?\s?(?<column>[\w\s]+)?\s?(?<direction>asc|desc|ascending|descending|highest|lowest)/imx
