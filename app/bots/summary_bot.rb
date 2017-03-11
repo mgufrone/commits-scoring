7d16e5718694959062d1e20e4e6a796c1478c665
Mochamad Gufron <mgufronefendi@gmail.com>
2017-03-11 16:29:17 +0700

    update pattern summary

--- app/bots/summary_bot.rb
+++ app/bots/summary_bot.rb
@@ -3,7 +3,7 @@ class SummaryBot
         @client = client
     end
     def pattern
-        /(?<action>summary|report)\s?|(?<user>my)?\s?(performance\s(of|for)?(?<user>[\@a-zA-Z\d]+))/imx
+        /(?<action>summary|report)\s?|(?<user>my)?\s?(performance\s(of|for)\s(?<user>[\@a-zA-Z\d]+))/imx
     end
     def order_pattern
         /(order|sort)\s?by?\s?(?<column>[\w\s]+)?\s?(?<direction>asc|desc|ascending|descending|highest|lowest)/imx
