96f7937708dd9bcb98e795eef53e913cb47a2ca8
Mochamad Gufron <mgufronefendi@gmail.com>
2017-03-11 21:30:54 +0700

    fix bug

--- app/bots/help_bot.rb
+++ app/bots/help_bot.rb
@@ -7,17 +7,17 @@ class HelpBot
     end
     def process(message, data)
         @client.typing
-        [
+        message = [
             "Try this command:",
             "`show me summary/report (for today, yesterday)`",
             "It will show you the commit summary report",
             "show me performance of (user)",
             "it will show you the performance of specific person",
             "count all commits (for|at|between) (date|start to end) ",
-            "it will show you of total number of commits"
+            "it will show you of total number of commits",
             "score/mark commit_id to score_value: ",
             "this will do to mark a commit and score it out"
-        ]
-        @client.send text: "<@#{data.user}> Hai there!", channel: data.channel
+        ].join("\n")
+        @client.send text: "<@#{data.user}>\n #{message}", channel: data.channel
     end
 end
